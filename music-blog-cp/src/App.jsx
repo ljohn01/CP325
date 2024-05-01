92027d11cfeca97ba318e362c32ed1f466488bdc
ljohn01 <01.lauren.johnson@gmail.com>
2024-05-01 14:22:27 -0400

    another commit

--- music-blog-cp/src/App.jsx
+++ music-blog-cp/src/App.jsx
@@ -1,35 +1,33 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Navbar from '../components/Navbar';  
 import './App.css'
+import About from '../components/About'
+import Blog from '../components/Blog';
+import Profile from '../components/Profile';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  let Component
+  switch (window.location.pathname) {
+    case "/home":
+      Component = App
+    break
+    case "/about":
+      Component = About
+      break
+      case "/blog":
+        Component = Blog
+        break
+        case "/profile":
+          Component = Profile
+          break
+  }
+return (
+  <> 
+  <Navbar />
+  <Component /> 
+  </>
+ 
+)
 }
 
 export default App
