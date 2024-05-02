90ad0f93f56aa8588a51f52fdf8914df632a44c4
ljohn01 <01.lauren.johnson@gmail.com>
2024-05-02 13:48:38 -0400

    another commit

--- music-blog-cp/src/App.jsx
+++ music-blog-cp/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from './components/Navbar';
+import Posts from './components/Posts';
 
 const App = () => {
 
 return (
   <div className='container'>
    <Navbar/>
+  
   </div>
   );
 }
--- music-blog-cp/src/components/Navbar.jsx
+++ music-blog-cp/src/components/Navbar.jsx
@@ -3,23 +3,25 @@ import './Navbar.css'
 import mainLogo from '../images/harmonic-haven.png'
 
 
+
 const Navbar = () => {
   return (
     <div className='navbar'>
      <img src={mainLogo} alt="Harmonic Haven Logo" className='logo'/>
      <ul>
-        <li href="/">Home</li>
-        <li href="/about">About</li>
-        <li href="/blog">Blog</li>
-        <li href="/profile">Profile</li>
+        <li> <a href="/">Home</a></li>
+        <li> <a href="about">About</a></li>
+        <li> <a href="/blog">Blog</a></li>
+        <li> <a href="/profile">Profile</a></li>
      </ul>
 
+ 
 <div className='search-box'>
     <input type="text" placeholder='Search'/>
     <img src="" alt=""/>
 </div>
 
-<img src="" alt="" className='toggle-icon'/>
+
 
     </div>
   )
